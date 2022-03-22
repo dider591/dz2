53651900bbcc5980d448f485de3fcf37bf840519
ildar v <genanapolotence@gmail.com>
2022-03-22 19:50:53 +0500

    Detail/94

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+
+    private Animator _animator;
+    private SpriteRenderer _spriteRenderer;
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        _animator.SetBool("Walk", false);
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            _spriteRenderer.flipX = false;
+            _animator.SetBool("Walk", true);
+            transform.Translate(_speed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            _spriteRenderer.flipX = true;
+            _animator.SetBool("Walk", true);
+            transform.Translate(_speed * Time.deltaTime * - 1, 0, 0);
+        }
+    }
+}

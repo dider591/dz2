1a64c8ec7836ac6526e336f485eacd9b7ce129d5
ildar v <genanapolotence@gmail.com>
2022-03-23 18:04:22 +0500

    23.03
    23.03

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(SpriteRenderer))]
+
 public class Movement : MonoBehaviour
 {
     private const string Walk = "Walk";
